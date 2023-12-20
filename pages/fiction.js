4ceb9aa8c8c718510e68d43b3010a129869a2d81
ratandeep1008 <rbulbake3@gmail.com>
2023-12-20 13:48:13 +0530

    review section added

--- pages/fiction.js
+++ pages/fiction.js
@@ -1,3 +1,7 @@
+let wishlistStatus = true;
+let wishlistAllStatus = true;
+
+
 const quantityElement = document.getElementById("quantity-text");
 function increaseQuantity() {
     let quantity = parseInt(quantityElement.innerText);
@@ -18,5 +22,33 @@ function decreaseQuantity() {
     }
     else {
         alert("You can't add less than 1 item");
+    }
+}
+
+function wishlistHeart() {
+    console.log(wishlistStatus);
+    const wishlistElement = document.getElementById('wishlistIconId');
+
+    if(wishlistStatus){
+        wishlistElement.src= "./../images/home/heart-red.png";
+        wishlistStatus = false;
+    }
+    else{
+        wishlistElement.src= "./../images/home/heart.png";
+        wishlistStatus = true;
+    }
+}
+
+function wishlistHeartAll(imgId){
+    console.log(imgId);
+    const wishlistElement = document.getElementById(imgId);
+
+    if(wishlistAllStatus){
+        wishlistElement.src= "./../images/home/heart-red.png";
+        wishlistAllStatus = false;
+    }
+    else{
+        wishlistElement.src= "./../images/home/heart.png";
+        wishlistAllStatus = true;
     }
 }
